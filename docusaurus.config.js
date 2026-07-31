8ef534200d896ad938a1acc939754fde735a5f3b
MichaelO <michael811125@gmail.com>
2026-07-31 12:40:14 +0800

    site: set production url to oxgkit.ouhiyo.com (fix sitemap domain)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,7 +35,7 @@ const config = {
   ],
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://oxgkit.ouhiyo.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   //baseUrl: '/build/',
